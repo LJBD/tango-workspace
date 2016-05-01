b9dacc587b2a07074bd971af1eaa86b0cc14baf2
mliszcz <liszcz.michal@gmail.com>
2016-05-01 23:51:38 +0200

    classpaths updated in java tools; mysql upgraded to 5.7

--- run-db.sh
+++ run-db.sh
@@ -6,5 +6,5 @@ docker run -it --name mysql_db --rm \
   -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_DATABASE=tango_db -e MYSQL_USER=tango -e MYSQL_PASSWORD=tango \
   -v $PWD/database/install_scripts:/docker-entrypoint-initdb.d:ro \
   -v $PWD/.data/mysq:/var/lib/mysql \
-  mysql:5.6 \
+  mysql:5.7 \
   --sql-mode=""
