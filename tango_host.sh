LJBD/tango-workspace

commit 744ef2b94c25d79623fff290c9ad78707807acf3

updated for centos-based tango image

tango_host.sh
@@ -7,7 +7,7 @@ QUERY_PORT='{{ range $key, $value := .NetworkSettings.Ports }}{{ $key }}{{ end }
 QUERY_HOST='{{ .Config.Hostname }}'
 QUERY_DOMAIN='{{ .Config.Domainname }}'
 
-CONTAINER=tangoworkspace_databaseds_1
+CONTAINER=tangocsworkspace_databaseds_1
 
 IPADDR=`docker inspect -f "$QUERY_IP" $CONTAINER`
 TCPPORT=`docker inspect -f "$QUERY_PORT" $CONTAINER`

.config/supervisord/supervisord.databaseds.conf
@@ -4,7 +4,7 @@ logfile=/var/tmp/supervisord.log
 pidfile=/var/tmp/supervisord.pid
 
 [program:tango-db]
-command=/usr/local/bin/DataBaseds 2 -ORBendPoint giop:tcp::10000
+command=/usr/bin/DataBaseds 2 -ORBendPoint giop:tcp::10000
 autorestart=true
 startretries=5
 priority=1
@@ -12,7 +12,7 @@ priority=1
 [program:tango-starter]
 command=/bin/bash -c '
   /usr/local/bin/tango_register_device Starter/`hostname --short` Starter tango/admin/`hostname --short` &&
-  exec -- /usr/local/bin/Starter `hostname --short`'
+  exec -- /usr/bin/Starter `hostname --short`'
 autorestart=true
 startretries=5
 priority=2
@@ -20,7 +20,7 @@ priority=2
 [program:tango-accesscontrol]
 command=/bin/bash -c '
   /usr/local/bin/tango_register_device TangoAccessControl/1 TangoAccessControl sys/access_control/1 &&
-  exec -- /usr/local/bin/TangoAccessControl 1'
+  exec -- /usr/bin/TangoAccessControl 1'
 autorestart=true
 startretries=5
 priority=3

.config/supervisord/supervisord.tangotest.conf
@@ -6,7 +6,7 @@ pidfile=/var/tmp/supervisord.pid
 [program:tango-starter]
 command=/bin/bash -c '
   /usr/local/bin/tango_register_device Starter/`hostname --short` Starter tango/admin/`hostname --short` &&
-  exec -- /usr/local/bin/Starter `hostname --short`'
+  exec -- /usr/bin/Starter `hostname --short`'
 autorestart=true
 startretries=5
 priority=1
@@ -14,7 +14,7 @@ priority=1
 [program:tango-test]
 command=/bin/bash -c '
   /usr/local/bin/tango_register_device TangoTest/test TangoTest sys/tg_test/1 &&
-  exec -- /usr/local/bin/TangoTest test'
+  exec -- /usr/bin/TangoTest test'
 autorestart=true
 startretries=5
 priority=5

docker-compose.yml
@@ -2,7 +2,7 @@ version: '2'
 services:
 
   mysql:
-    image: mysql:5.7
+    image: mysql:5.6
     hostname: mysql.workspace
     command: mysqld --sql-mode=""
     environment:
@@ -15,7 +15,7 @@ services:
       # - ./.data/mysql:/var/lib/mysql
 
   databaseds:
-    image: mliszcz/tango-cs:xenial
+    image: tangocs/tango-cs:9
     hostname: databaseds.workspace
     command: >-
       /bin/sh -c 'wait-for-it.sh $${MYSQL_HOST} --timeout=30 --strict --
@@ -33,7 +33,7 @@ services:
       - mysql:mysql.workspace
 
   tangotest:
-    image: mliszcz/tango-cs:xenial
+    image: tangocs/tango-cs:9
     hostname: tangotest.workspace
     command: >-
       /bin/sh -c 'wait-for-it.sh $${TANGO_HOST} --timeout=30 --strict --

commit c6f99e1b97fe0e9b8cdb6d2a86001d8cce01b72e

fixed tango_host.sh (broken in 744ef2b)

tango_host.sh
@@ -7,7 +7,7 @@ QUERY_PORT='{{ range $key, $value := .NetworkSettings.Ports }}{{ $key }}{{ end }
 QUERY_HOST='{{ .Config.Hostname }}'
 QUERY_DOMAIN='{{ .Config.Domainname }}'
 
-CONTAINER=tangocsworkspace_databaseds_1
+CONTAINER=tangoworkspace_databaseds_1
 
 IPADDR=`docker inspect -f "$QUERY_IP" $CONTAINER`
 TCPPORT=`docker inspect -f "$QUERY_PORT" $CONTAINER`